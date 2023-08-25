28211a9ea464c1e1531f4735e54afebabf7f989b
Marko Duke <123811832+markodjukic96@users.noreply.github.com>
2023-08-25 12:31:47 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,86 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById(`nav-menu`);
+const navToogle = document.getElementById(`nav-toggle`);
+const navClose = document.getElementById(`nav-close`);
+
+if (navToogle) {
+  navToogle.addEventListener(`click`, () => {
+    navMenu.classList.add(`show-menu`);
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener(`click`, () => {
+    navMenu.classList.remove(`show-menu`);
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(`.nav__link`);
+
+const linkAction = () => {
+  const navMenu = document.getElementById(`nav-menu`);
+  navMenu.classList.remove(`show-menu`);
+};
+
+navLink.forEach((n) => n.addEventListener(`click`, linkAction));
+/*=============== ADD BLUR TO HEADER ===============*/
+
+const blurlHeader = () => {
+  const header = document.getElementById(`header`);
+
+  this.scrollY >= 50
+    ? header.classList.add(`blur-header`)
+    : header.classList.remove(`blur-header`);
+};
+
+window.addEventListener(`scroll`, blurHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById(`scroll-up`);
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add(`show-scroll`)
+    : scrollUp.classList.remove(`show-scroll`);
+};
+
+window.addEventListener(`scroll`, scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll(`sections[id]`);
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute(`id`),
+      sectionsClass = document.querySelector(
+        `.nav__menu a[href*=` + sectionId + `]`
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add(`active-link`);
+    } else {
+      sectionsClass.classList.remove(`active-link`);
+    }
+  });
+};
+
+window.addEventListener(`scroll`, scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 3000,
+  delay: 400,
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__card`, { delay: 600, distance: `100px`, interval: 100 });
